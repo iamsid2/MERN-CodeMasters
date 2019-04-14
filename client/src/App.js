357ee0b964c3e8cf86f037c88aa181e6ae8a9dfd
iamsid2 <shaktimund97@gmail.com>
2019-04-15 02:25:52 +0530

    Start Redux Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import Navbar from "./components/layout/Navbar.js";
 import Landing from "./components/layout/Landing.js";
 import Footer from "./components/layout/footer.js";
+import Login from "./components/auth/Login.js";
+import Register from "./components/auth/Register.js";
+import store from "./store";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
